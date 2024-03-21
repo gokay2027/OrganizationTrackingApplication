2407683bd1e653ad52c7b996abbb0bd8f9020512
gokay2027 <gokaydinc2000@gmail.com>
2024-03-21 22:09:13 +0300

    ..

--- OrganizationTrackingApplicationData/GenericRepository/Concrete/GenericRepository.cs
+++ OrganizationTrackingApplicationData/GenericRepository/Concrete/GenericRepository.cs
@@ -1,1 +1,39 @@
-﻿
+﻿using Entities.BaseAggregate.Concrete;
+using Microsoft.EntityFrameworkCore;
+using OrganizationTrackingApplicationData.GenericRepository.Abstract;
+
+namespace OrganizationTrackingApplicationData.GenericRepository.Concrete
+{
+    public class GenericRepostory : IGenericRepository<BaseEntity>
+    {
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<BaseEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public BaseEntity GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(BaseEntity obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(BaseEntity obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
