3eb3857fb8de51a9d8f5b5f349a3f978330dbccf
Gokay DINC <gokaydinc2000@gmail.com>
2024-05-06 11:02:12 +0300

    explanation of test data context factory

--- OrganizationTrackingApplicationTest/DataFactory/TestDataContextFactory.cs
+++ OrganizationTrackingApplicationTest/DataFactory/TestDataContextFactory.cs
@@ -1,9 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using OrganizationTrackingApplicationData;
 
+/// <summary>
+/// Using sqlite, it provides us relations between entities
+/// it is important that context should be used once actually the
+/// technique is creating the connection to inMemory database second,
+/// using context the in memory connection option should be used on the context,
+/// the context provides us the relations and entities. So we have a good options for
+/// both test and development databases. Hereby, thanks to TestContextFactory, we have both
+/// sqlite test database and except that, we have a normal database which comes from
+/// MSSQL
+/// </summary>
 public class TestDataContextFactory
 {
-
+    /// <summary>
+    /// Creates context for testing using sqlite
+    /// </summary>
+    /// <returns></returns>
     protected OrganizationTrackingApplicationDbContext GetContext()
     {
         var options = new DbContextOptionsBuilder<OrganizationTrackingApplicationDbContext>().
@@ -18,6 +31,10 @@ public class TestDataContextFactory
         return db;
     }
 
+    /// <summary>
+    /// Initial in memory data for testing
+    /// </summary>
+    /// <param name="context"></param>
     protected void InitialData(OrganizationTrackingApplicationDbContext context)
     {
         context.Users.AddAsync(new Entities.Domain.User("Gokay", "Dinç", "gokay123@gmail.com", "1234", true));
