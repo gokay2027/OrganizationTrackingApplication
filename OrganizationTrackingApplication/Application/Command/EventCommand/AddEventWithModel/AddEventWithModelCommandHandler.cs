af2be004e781cb5146529129e25fd0c232f5ccba
gokay2027 <gokaydinc2000@gmail.com>
2024-05-30 00:27:52 +0300

    ..

--- OrganizationTrackingApplication/Application/Command/EventCommand/AddEventWithModel/AddEventWithModelCommandHandler.cs
+++ OrganizationTrackingApplication/Application/Command/EventCommand/AddEventWithModel/AddEventWithModelCommandHandler.cs
@@ -2,21 +2,22 @@
 using MediatR;
 using OrganizationTrackingApplicationApi.Model.Event.AddEventWithModelCommand;
 using OrganizationTrackingApplicationData.GenericRepository.Abstract;
-using System.Data.Entity;
 
 namespace OrganizationTrackingApplicationApi.Application.Command.EventCommand.AddEventWithModel
 {
     public class AddEventWithModelCommandHandler : IRequestHandler<AddEventWithModelCommand, AddEventWithModelOutputModel>
     {
         private readonly IGenericRepository<User> _userRepository;
-        private readonly IGenericRepository<Location> _locationRepository;
         private readonly IGenericRepository<Event> _eventRepository;
+        private readonly IGenericRepository<Organizator> _organizatorRepository;
+        private readonly IGenericRepository<Location> _locationRepository;
 
-        public AddEventWithModelCommandHandler(IGenericRepository<User> userRepository, IGenericRepository<Location> locationRepository, IGenericRepository<Event> eventRepository)
+        public AddEventWithModelCommandHandler(IGenericRepository<User> userRepository, IGenericRepository<Event> eventRepository, IGenericRepository<Organizator> organizatorRepository, IGenericRepository<Location> locationRepository)
         {
             _userRepository = userRepository;
-            _locationRepository = locationRepository;
             _eventRepository = eventRepository;
+            _organizatorRepository = organizatorRepository;
+            _locationRepository = locationRepository;
         }
 
         public async Task<AddEventWithModelOutputModel> Handle(AddEventWithModelCommand request, CancellationToken cancellationToken)
@@ -25,13 +26,17 @@ namespace OrganizationTrackingApplicationApi.Application.Command.EventCommand.Ad
             {
                 var locationToBeAdded = new Location(request.AddressDescription, request.FormattedAddressName, request.Latitude, request.Longitude);
 
-                var userSet = await _userRepository.GetSet();
+                await _locationRepository.Insert(locationToBeAdded);
 
-                var user = userSet.Include(u => u.Organizator).FirstOrDefault(a => a.Id.Equals(request.CreatedById));
+                var user = await _userRepository.GetById(request.CreatedById);
+
+                var organizatorResult = await _organizatorRepository.GetByFilter(a => a.UserId.Equals(user.Id));
+
+                var organizator = organizatorResult.FirstOrDefault();
 
                 DateTime eventDate = DateTime.Parse(request.EventTime, null, System.Globalization.DateTimeStyles.RoundtripKind);
 
-                var eventToBeAdded = new Event(request.EventName, eventDate, locationToBeAdded.Id, request.EventTypeId, user.Organizator.Id, request.TicketPrice, request.TicketNumber);
+                var eventToBeAdded = new Event(request.EventName, eventDate, locationToBeAdded.Id, request.EventTypeId, organizator.Id, request.TicketPrice, request.TicketNumber);
 
                 await _eventRepository.Insert(eventToBeAdded);
 
@@ -45,8 +50,8 @@ namespace OrganizationTrackingApplicationApi.Application.Command.EventCommand.Ad
             {
                 return new AddEventWithModelOutputModel
                 {
-                    IsSuccess = true,
-                    Message = ex.Message
+                    IsSuccess = false,
+                    Message = ex.InnerException.ToString()
                 };
             }
         }
