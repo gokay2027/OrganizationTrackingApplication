7dff243a9fb41f70c566cbc6c0a689ab82a8100b
gokay2027 <gokaydinc2000@gmail.com>
2024-05-30 23:13:23 +0300

    ..

--- OrganizationTrackingApplication/Model/Event/GetEvents/EventListModel.cs
+++ OrganizationTrackingApplication/Model/Event/GetEvents/EventListModel.cs
@@ -9,13 +9,20 @@ namespace OrganizationTrackingApplicationApi.Model.Event.GetEvents
 
     public class EventListItem
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public DateTime EventTime { get; set; }
+
+        public string EventDetail { get; set; }
+        public Guid CreatorId { get; set; }
+        public string CreatorNameSurname { get; set; }
+        public float Longitude { get; set; }
+        public float Latitude { get; set; }
+
         public string EventTypeName { get; set; }
         public string OrganizatorName { get; set; }
         public string LocationAdress { get; set; }
         public bool IsCompleted { get; set; }
         public List<string> Rules { get; set; } = new List<string>();
-
     }
 }
